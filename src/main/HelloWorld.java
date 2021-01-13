8792e9c22589546cb112f081a736c2284ad85368
gmadkat <gautam.subbacharya@gmail.com>
2021-01-13 17:53:37 -0500

    Move to source

---
+++ src/main/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        String printHello = "Hello, World!";
+	System.out.println(printHello);
+    }
+
+}
+
